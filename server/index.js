2b7c4c7c4a06ecfd492f42e874deb89e8ec134be
at1208 <amantiwari12081999@gmail.com>
2020-01-25 23:53:28 +0530

    chalk added and db connected

--- server/index.js
+++ server/index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const chalk = require('chalk');
 
 
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 
-mongoose.connect('')
-   .then(() => console.log(`Connected to MongoDB`));
-   .catch( error => console.log(`ERROR: ${error}`));
 
 const Port = process.env.PORT || 5000;
+app.listen(Port, () => console.log(chalk.blue.bold(`Listeniing to ${chalk.red.bold(Port)}`)));
 
-app.listen(Port, () => console.log(`Listeniing to ${Port}`));
+
+
+
+mongoose.connect(process.env.MONGODB, { useUnifiedTopology: true, useNewUrlParser: true })
+   .then(
+      () => console.log(chalk.blue.bold(`Connected to MongoDB`)
+   )
+ )
+   .catch(
+     (error) => console.log(chalk.red.bold(`ERROR: ${error}`)
+   )
+ );
